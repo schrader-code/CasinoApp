69a532e0660baac1e395b08860d892f4d28825d4
Louis <diegopc500lol@gmail.com>
2025-08-08 23:19:38 -0600

    Versión con MariaDB y Lobby funcional

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -2,11 +2,23 @@ import mysql from "mysql2/promise";
 import dotenv from "dotenv";
 dotenv.config();
 
+console.log("DB env ->", {
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  hasPass: !!process.env.DB_PASSWORD,   // no imprime la pass, solo si existe
+  db: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+});
+
+
+
 export const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
-  password: process.env.DB_PASS,
+  password: process.env.DB_PASSWORD,   // <-- AQUÍ
   database: process.env.DB_NAME,
+  port: Number(process.env.DB_PORT || 3306),
   waitForConnections: true,
   connectionLimit: 10,
 });
+
